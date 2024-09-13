d4e01705c04dee235bbe46dec392ea73ca7ca27e
Nina <2681120@schoolsnet.act.edu.au>
2024-09-13 21:39:21 +1000

    Adding game manager script

---
+++ GameManagerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public GameObject gameOverUI; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void gameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
